998075d254a1ddf12b64aac416ad0dcc3fc1eb6c
Vinicius Garcia <34985672+Vinicius-Garcia@users.noreply.github.com>
2021-09-16 07:26:53 -0300

    format code

--- Sistema_Lixeira/Sistema_Lixeira.ino
+++ Sistema_Lixeira/Sistema_Lixeira.ino
@@ -1,17 +1,24 @@
+
+//declarações de variaveis (portas usadas)
+
 int rele1 = 2;
 int rele2 = 3;
 int rele3 = 4;
 int sensorBoia = 5;
+
+
 void setup() {
   Serial.begin(9600);
+  //seta o tipo da porta
   pinMode(rele1, OUTPUT);
   pinMode(rele2, OUTPUT);
   pinMode(rele3, OUTPUT);
   pinMode(sensorBoia, INPUT_PULLUP);
 }
 void loop() {
-  int estado = digitalRead (sensorBoia);
-if(estado == 1){//testa pra ver se o sensor de nivel está com agua
+
+int estado = digitalRead (sensorBoia);
+if(estado){//testa pra ver se o sensor de nivel está com agua
   if(!Serial.available()){
     Start:
      Serial.println("Escolha:");
@@ -25,51 +32,47 @@ if(estado == 1){//testa pra ver se o sensor de nivel está com agua
       digitalWrite(rele2,HIGH);
       delay(1000);
       digitalWrite(rele2,LOW);
-       if(!Serial.available()){
-        Serial.println("Escolha:");
-       Serial.println("1 - Ligar Protótipo");
-       Serial.println("2 - Voltar Para o inicio");
-       int on = Serial.read();
-
-             if(on == 1){
-              Serial.println("Ligando Protótipo . . . ");
-              digitalWrite(rele1,HIGH);
-              delay(4000);
-              digitalWrite(rele1,LOW);
-             }
-             if(on == 2){
+      if(!Serial.available()){
+           Serial.println("Escolha:");
+           Serial.println("1 - Ligar Protótipo");
+           Serial.println("2 - Voltar Para o inicio");
+           int on = Serial.read();
+           if(on == 1){
+                  Serial.println("Ligando Protótipo . . . ");
+                  digitalWrite(rele1,HIGH);
+                  delay(4000);
+                  digitalWrite(rele1,LOW);
+           }if(on == 2){
               goto Start;
-             }
+           }
        }
      }
      if (c == 2){
-      Serial.println("Colocando produto 2 . . . ");//se a pessoa escolheu o produto 2 liga o rele por 2 seg
-      digitalWrite(rele2,HIGH);
-      delay(2000);
-      digitalWrite(rele2,LOW);
-       if(!Serial.available()){//escolhe se quer ligar ou ir pro inicio
-        Serial.println("Escolha:");
-       Serial.println("1 - Ligar Protótipo");
-       Serial.println("2 - Voltar Para o inicio");
-       int on = Serial.read();
-
-             if(on == 1 ){
-              Serial.println("Ligando Protótipo . . . ");
-              digitalWrite(rele1,HIGH);
-              delay(4000);
-              digitalWrite(rele1,LOW);
-             }
-             if(on == 2){
-              goto Start;
-             }
-       }
+          Serial.println("Colocando produto 2 . . . ");//se a pessoa escolheu o produto 2 liga o rele por 2 seg
+          digitalWrite(rele2,HIGH);
+          delay(2000);
+          digitalWrite(rele2,LOW);
+          if(!Serial.available()){//escolhe se quer ligar ou ir pro inicio
+              Serial.println("Escolha:");
+              Serial.println("1 - Ligar Protótipo");
+              Serial.println("2 - Voltar Para o inicio");
+              int on = Serial.read();
+                  if(on == 1 ){
+                      Serial.println("Ligando Protótipo . . . ");
+                      digitalWrite(rele1,HIGH);
+                      delay(4000);
+                      digitalWrite(rele1,LOW);
+                  }if(on == 2){
+                    goto Start;
+                  }
+          }
      }
      if (c == 3){
-       if(!Serial.available()){//escolhe se quer ligar ou ir pro inicio
-        Serial.println("Escolha:");
-       Serial.println("1 - Ligar Protótipo");
-       Serial.println("2 - Voltar Para o inicio");
-       int on = Serial.read();
+        if(!Serial.available()){//escolhe se quer ligar ou ir pro inicio
+          Serial.println("Escolha:");
+          Serial.println("1 - Ligar Protótipo");
+          Serial.println("2 - Voltar Para o inicio");
+          int on = Serial.read();
              if(on == 1){
               Serial.println("Ligando Protótipo . . . ");
               digitalWrite(rele1,HIGH);
